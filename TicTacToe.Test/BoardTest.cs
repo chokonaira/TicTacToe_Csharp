105c486e0f6898bd9e9621a3c5781b799fa0d7d8
Henry Okonkwo <okonkwo.henry2012@gmail.com>
2020-04-11 17:03:45 +0200

    implemented check for draw method

--- TicTacToe.Test/BoardTest.cs
+++ TicTacToe.Test/BoardTest.cs
@@ -47,7 +47,6 @@ namespace TicTacToe.Test
             board.MakeMove('o', 0);
             int expected = 8;
             Assert.Equal(expected, board.GetAvailableMoves());
-
         }
 
         [Fact]
@@ -58,7 +57,6 @@ namespace TicTacToe.Test
             board.GameBoard[1] = 'x';
             board.GameBoard[2] = 'x';
             bool actual = board.CheckWin('x');
-
             Assert.True(actual);
 
         }
@@ -71,9 +69,7 @@ namespace TicTacToe.Test
             board.GameBoard[4] = 'o';
             board.GameBoard[7] = 'o';
             bool actual = board.CheckWin('o');
-
             Assert.True(actual);
-
         }
 
         [Fact]
@@ -84,118 +80,78 @@ namespace TicTacToe.Test
             board.GameBoard[4] = 'x';
             board.GameBoard[6] = 'x';
             bool actual = board.CheckWin('x');
-
             Assert.True(actual);
 
         }
+
         [Fact]
-        public void CheckBoardForDraw()
+        public void CheckBoardForSecondDiagonalWin()
         {
-         
             Board board = new Board(3);
-            char[] actual = board.positions;
-            char[] expected = { '1', '2', '3', '4', '5', '6', '7', '8', '9' };
-
-            Assert.Equal(expected, actual);
+            board.GameBoard[0] = 'x';
+            board.GameBoard[4] = 'x';
+            board.GameBoard[8] = 'x';
+            bool actual = board.CheckWin('x');
+            Assert.True(actual);
         }
-        //[Fact]
-        //public void SetPositionX()
-        //{
-        //    Board board = new Board();
-        //    board.SetPositions(1, 'x');
 
-        //    char[] actual = board.positions;
-        //    char[] expected = { 'x', '2', '3', '4', '5', '6', '7', '8', '9' };
+        [Fact]
+        public void CheckForActualDraw()
+        {
+            Board board = new Board(3);
+            // o o x
+            // x x o
+            // o x x
+            board.GameBoard[0] = 'o';
+            board.GameBoard[1] = 'o';
+            board.GameBoard[2] = 'x';
+            board.GameBoard[3] = 'x';
+            board.GameBoard[4] = 'x';
+            board.GameBoard[5] = 'o';
+            board.GameBoard[6] = 'o';
+            board.GameBoard[7] = 'x';
+            board.GameBoard[8] = 'x';
+            bool actual = board.CheckDraw('x', 'o');
+            Assert.True(actual);
+        }
 
-        //    Assert.Equal(expected, actual);
-        //}
+        [Fact]
+        public void FalseDrawIfWin()
+        {
+            Board board = new Board(3);
+            // x o x
+            // x x o
+            // o x x
+            board.GameBoard[0] = 'x';
+            board.GameBoard[1] = 'o';
+            board.GameBoard[2] = 'x';
+            board.GameBoard[3] = 'x';
+            board.GameBoard[4] = 'x';
+            board.GameBoard[5] = 'o';
+            board.GameBoard[6] = 'o';
+            board.GameBoard[7] = 'x';
+            board.GameBoard[8] = 'x';
+            bool actual = board.CheckDraw('x', 'o');
+            Assert.False(actual);
+        }
 
-        //[Fact]
-        //public void SetPositionO()
-        //{
-        //    Board board = new Board();
-        //    board.SetPositions(1, 'o');
-
-        //    char[] actual = board.positions;
-        //    char[] expected = { 'o', '2', '3', '4', '5', '6', '7', '8', '9' };
-
-        //    Assert.Equal(expected, actual);
-        //}
-
-        //[Fact]
-        //public void SetPositionXinMiddle()
-        //{
-        //    Board board = new Board();
-        //    board.SetPositions(2, 'x');
-
-        //    char[] actual = board.positions;
-        //    char[] expected = { '1', 'x', '3', '4', '5', '6', '7', '8', '9' };
-
-        //    Assert.Equal(expected, actual);
-        //}
-
-        //[Fact]
-        //public void SetInitialBoard()
-        //{
-        //    Board board = new Board();
-        //    string actual = board.GetBoard();
-        //    string expected = "     |     |     \n" +
-        //                      "  1  |  2  |  3\n" +
-        //                      "_____|_____|_____\n" +
-        //                      "     |     |     \n" +
-        //                      "  4  |  5  |  6\n" +
-        //                      "_____|_____|_____\n" +
-        //                      "     |     |     \n" +
-        //                      "  7  |  8  |  9\n" +
-        //                      "     |     |     \n";
-
-        //    Assert.Equal(expected, actual);
-        //}
-
-        //[Fact]
-        //public void GetBoardWithPositionX()
-        //{
-        //    Board board = new Board();
-        //    board.SetPositions(1, 'x');
-        //    string actual = board.GetBoard();
-        //    string expected = "     |     |     \n" +
-        //                      "  x  |  2  |  3\n" +
-        //                      "_____|_____|_____\n" +
-        //                      "     |     |     \n" +
-        //                      "  4  |  5  |  6\n" +
-        //                      "_____|_____|_____\n" +
-        //                      "     |     |     \n" +
-        //                      "  7  |  8  |  9\n" +
-        //                      "     |     |     \n";
-
-        //    Assert.Equal(expected, actual);
-        //}
-
-        //[Fact]
-        //public void GetBoardWithAllPositions()
-        //{
-        //    Board board = new Board();
-        //    board.SetPositions(1, 'x');
-        //    board.SetPositions(2, 'o');
-        //    board.SetPositions(3, 'x');
-        //    board.SetPositions(4, 'o');
-        //    board.SetPositions(5, 'x');
-        //    board.SetPositions(6, 'o');
-        //    board.SetPositions(7, 'x');
-        //    board.SetPositions(8, 'o');
-        //    board.SetPositions(9, 'x');
-        //    string actual = board.GetBoard();
-        //    string expected = "     |     |     \n" +
-        //                      "  x  |  o  |  x\n" +
-        //                      "_____|_____|_____\n" +
-        //                      "     |     |     \n" +
-        //                      "  o  |  x  |  o\n" +
-        //                      "_____|_____|_____\n" +
-        //                      "     |     |     \n" +
-        //                      "  x  |  o  |  x\n" +
-        //                      "     |     |     \n";
-
-        //    Assert.Equal(expected, actual);
-        //}
+        [Fact]
+        public void FalseDrawIfBoardHasAvailableMoves()
+        {
+            Board board = new Board(3);
+            // - o x
+            // x x o
+            // o x x
+            board.GameBoard[1] = 'o';
+            board.GameBoard[2] = 'x';
+            board.GameBoard[3] = 'x';
+            board.GameBoard[4] = 'x';
+            board.GameBoard[5] = 'o';
+            board.GameBoard[6] = 'o';
+            board.GameBoard[7] = 'x';
+            board.GameBoard[8] = 'x';
+            bool actual = board.CheckDraw('x', 'o');
+            Assert.False(actual);
+        }
     }
 }
--- TicTacToe/Board.cs
+++ TicTacToe/Board.cs
@@ -5,25 +5,23 @@ namespace TicTacToe
     public class Board
     {
         public char[] GameBoard { get; set; }
-        //public char[] positions = { '1', '2', '3', '4', '5', '6', '7', '8', '9' };
 
         public Board(int size)
         {
             GameBoard = new char[size * size];
-            DisplayBoard();
+            InitializeBoard();
         }
 
-        public void MakeMove(char symbol, int position)
-        {
-            GameBoard[position] = symbol;
-        }
-
-        public void DisplayBoard()
+        public void InitializeBoard()
         {
             for (int i = 0; i < GameBoard.Length; i++)
             {
                 MakeMove(' ', i);
             }
+        }
+        public void MakeMove(char symbol, int position)
+        {
+            GameBoard[position] = symbol;
         }
 
         public int GetAvailableMoves()
@@ -38,16 +36,7 @@ namespace TicTacToe
             }
             return counter;
         }
-        //check for available spots and returns an int<< the state of the board
-        //once i make a move
-        //number of available spots will change
-        //evrytime u make a move, i will check if the
-        //
-
-        //val Gamboard[0]
-        //[0, 1, 2]
-        //[3, 4, 5]
-        //[6, 7, 8]
+       
         public bool CheckWin(char symbol)
         {
             return  (GameBoard[0] == symbol && GameBoard[1] == symbol && GameBoard[2] == symbol) ||
@@ -58,37 +47,14 @@ namespace TicTacToe
                     (GameBoard[2] == symbol && GameBoard[5] == symbol && GameBoard[8] == symbol) ||
                     (GameBoard[0] == symbol && GameBoard[4] == symbol && GameBoard[8] == symbol) ||
                     (GameBoard[2] == symbol && GameBoard[4] == symbol && GameBoard[6] == symbol);
-
         }
 
-        public bool CheckGameEnded(GetAvailableMoves)
+        public bool CheckDraw(char symbol1, char symbol2)
         {
-            if (int GetAvailableMoves() && CheckWin(char symbol))
-            {
-                return true;
-            }
-            return false;
+            if (CheckWin(symbol1) || CheckWin(symbol2)) return false;
+            if (GetAvailableMoves() > 0) return false;
+            return true;
         }
-
-
-        //public void SetPositions(int num, char value)
-        //{
-        //    positions[num - 1] = value;
-        //}
-
-        //public string GetBoard()
-        //{
-        //    return String.Format(EmptyBoard,
-        //         positions[0],
-        //         positions[1],
-        //         positions[2],
-        //         positions[3],
-        //         positions[4],
-        //         positions[5],
-        //         positions[6],
-        //         positions[7],
-        //         positions[8]);
-        //}
 
     }
 }
